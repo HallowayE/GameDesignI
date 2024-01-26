58d6cdc3721ff6f60998c15ce3bd5a62f96bb28d
HallowayE <112715037+HallowayE@users.noreply.github.com>
2024-01-26 13:17:52 -0600

    update

---
+++ prog54c.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+func _on_btn_calc_pressed():
+	var rad = float($lneRad.text)
+	var area = PI*pow(rad, 2)
+	var circ = 2*PI*rad
+	$lblArea.text = "Area: "+str(snapped(area, 0.001))
+	$lblCirc.text = "Circuferance: "+str(snapped(circ, 0.001))
+
+
+func _on_btn_clear_pressed():
+	$lblArea.text = "Area: "
+	$lblCirc.text = "Circumferance: " 
+	$lneRad.text = ""
+
+
+func _on_btn_exit_pressed():
+	get_tree().quit()
