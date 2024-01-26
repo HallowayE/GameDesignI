7a8d87c057fef024b1f08610247dcfe4beb6f8f6
HallowayE <112715037+HallowayE@users.noreply.github.com>
2024-01-26 13:02:41 -0600

    update

--- prog_54b.gd
+++ prog_54b.gd
@@ -1,11 +1,25 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_btn_calc_pressed():
+	var num1 = int($lne1.text)
+	var num2 = int($lne2.text)
+	var num3 = int($lne3.text)
+	var num4 = int($lne4.text)
+	var sum = num1+num2+num3+num4
+	var ave = float(sum)/4
+	$lblSum.text = "Sum: "+str(sum)
+	$lblAve.text = "Average: "+str(ave)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_btn_clear_pressed():
+	$lblSum.text = "Sum: "
+	$lblAve.text = "Average: " 
+	$lne1.text = ""
+	$lne2.text = ""
+	$lne3.text = ""
+	$lne4.text = ""
+
+
+func _on_btn_exit_pressed():
+	get_tree().quit()
