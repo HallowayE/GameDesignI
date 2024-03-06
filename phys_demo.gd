63c01b87bc41c6764b9ab4d7a9b1c49021b6818d
HallowayE <112715037+HallowayE@users.noreply.github.com>
2024-03-06 13:44:36 -0600

    update

--- phys_demo.gd
+++ phys_demo.gd
@@ -9,6 +9,12 @@ func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		var nball = preload("res://phys_ball.tscn").instantiate()
 		nball.position=event.position
+		var v_inertia = float($Panel/txtInrtia.text)
+		var v_x = float($Panel/txtVX.text)
+		var v_y = float($Panel/txtVY.text)
+		var velocity = Vector2(v_x, v_y)
+		nball.inertia=v_inertia
+		nball.linear_velocity=velocity
 		add_child(nball)
 
 
@@ -23,3 +29,17 @@ func _on_button_pressed():
 		if child is RigidBody2D:
 			var force = randi_range(-1000, 1000)
 			child.apply_central_impulse(Vector2(force,randi_range(-1000, 1000)))
+
+
+func _on_button_2_pressed():
+	var v_inertia = float($Panel/txtInrtia.text)
+	var v_x = float($Panel/txtVX.text)
+	var v_y = float($Panel/txtVY.text)
+	var velocity = Vector2(v_x, v_y)
+	for child in get_children():
+		if child is RigidBody2D:
+			child.inertia= v_inertia
+			child.linear_velocity=velocity
+			
+			
+
