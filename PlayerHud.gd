a761e341595b77eb3311bf6c847236aee8e77bcb
HallowayE <112715037+HallowayE@users.noreply.github.com>
2024-03-26 13:38:53 -0500

    update

--- PlayerHud.gd
+++ PlayerHud.gd
@@ -17,7 +17,7 @@ func create_heart():
 func draw_hearts():
 	for heart in hearts.get_children():
 		hearts.remove_child(heart)
-	for i in range(int(player.data.max_health)/20):
+	for i in range(int(player.data.max_health) / 20):
 		create_heart()
 		
 
@@ -26,17 +26,22 @@ func _ready():
 
 
 func _process(delta):
+	$Money/Coins.text = "%03d" % player.data.money
 	var p_health = player.data.health
-	var full_hearts = floor(p_health/20)
-	var remainder = int(p_health)%20
+	var full_hearts = floor(p_health / 20)
+	var remainder = int(p_health) % 20
+	
 	for heart in hearts.get_children():
 		var index = heart.get_index()
-		var x = (index % HEART_ROW_SIZE)*HEART_OFFSET
-		var y = (index / HEART_ROW_SIZE)*HEART_OFFSET
+		var x = (index % HEART_ROW_SIZE) * HEART_OFFSET
+		var y = (index / HEART_ROW_SIZE) * HEART_OFFSET
 		heart.position = Vector2(x, y)
-		if index>full_hearts:
+		
+		# Frame 8 empty, 7 1/4, 6 half, 5 3/4, 4 full
+		if index > full_hearts:
 			heart.frame = 8
-		elif index==full_hearts:
-			heart.frame = 8 - int(remainder/5)
+		elif index == full_hearts:
+			heart.frame = 8 - int(remainder / 5)
 		elif index < full_hearts:
 			heart.frame = 4
+	pass
