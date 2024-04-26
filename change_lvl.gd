7fafbe6ba7e1b0c7042b2971572165d983de9c90
HallowayE <112715037+HallowayE@users.noreply.github.com>
2024-04-26 13:44:35 -0500

    update

--- change_lvl.gd
+++ change_lvl.gd
@@ -6,5 +6,6 @@ extends Area2D
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		var lvl = "res://"+next_level+".tscn"
+		player = get_tree().get_first_node_in_group("Player")
 		get_tree().change_scene_to_file(lvl)
-		body.global_position+=coord
+		get_tree().set_first_node_in_group("Player") = player
